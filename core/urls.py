075f09d68bc5eab7766a797e8201fcba8d3e9d08
Valney Filho <valneyf.dev@gmail.com>
2023-03-01 22:45:10 -0300

    Configurado para templates globais

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls'))
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
